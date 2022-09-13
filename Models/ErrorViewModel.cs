c2d0d4c5fe74ffac1f432bd76ee9d89d45b7d3dd
Marko762 <73674520+Marko762@users.noreply.github.com>
2022-09-13 17:03:01 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Readit.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
