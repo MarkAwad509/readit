bb89584e918f80f7f0d4b49441608987b3a296ca
Mark <markawad509@gmail.com>
2022-12-02 23:03:57 -0500

    *finished product*

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using Readit.Models;
 using Readit.Models.Entities;
 
 namespace Readit.Controllers {
     public class ProfileController : Controller {
+        private readonly mproulx_5w6_readitContext _context;
         private readonly ISession _session;
 
         public ProfileController(IHttpContextAccessor httpContextAccessor) {
+            _context = new mproulx_5w6_readitContext();
             _session = httpContextAccessor.HttpContext.Session;
         }
         public IActionResult Index() {
@@ -15,10 +18,18 @@ namespace Readit.Controllers {
 
         public IActionResult ProfileView() {
             var user = JsonConvert.DeserializeObject<Member>(_session.GetString("user"));
-            ViewBag.CommentCount = user.Comments.Count;
-            ViewBag.PostCount = user.Links.Count;
-            ViewBag.LikesCount = countScore(user);
-            ViewBag.user = user;
+            var currentUser = _context.Members.Find(user.Id);
+            if (user.Comments != null || user.Links != null || user.Votes != null){
+                ViewBag.CommentCount = currentUser.Comments.Count;
+                ViewBag.PostCount = currentUser.Links.Count;
+                ViewBag.LikesCount = countScore(currentUser);
+            } else {
+                ViewBag.CommentCount = 0;
+                ViewBag.PostCount = 0;
+                ViewBag.LikesCount = 0;
+            }
+            ViewBag.user = currentUser;
+            ViewBag.links = _context.Links.Where(l => l.MemberId == user.Id).OrderByDescending(l => l.PublicationDate).ToList();
             return View("ProfileView");
         }
 
